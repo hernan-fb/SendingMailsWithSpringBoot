df800b7498b00d72a46e033690b5acad8809b7a7
Hernan Brando - ZENI <hbrando@zeni.com.ar>
2024-07-16 00:59:01 -0300

    working with user and password on system environment variables

--- src/main/java/com/api/inutils/emailsender/EmailSenderApplication.java
+++ src/main/java/com/api/inutils/emailsender/EmailSenderApplication.java
@@ -1,12 +1,17 @@
 package com.api.inutils.emailsender;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@Import(EmailConfig.class)
 public class EmailSenderApplication {
 
 	public static void main(String[] args) {
+		/*Map<String, String> env = System.getenv();
+		env.forEach((key, value) -> {
+			System.out.println(key + ": " + value);
+		});
+		*/
 		SpringApplication.run(EmailSenderApplication.class, args);
 	}
 
